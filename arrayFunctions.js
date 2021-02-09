a62688608d1c29ecb852ea3ec7a5b7845abe856e
Mano <apple@Macintosh.local>
2021-02-09 09:48:22 +0300

    Done

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -9,8 +9,13 @@
  * getOdds([11, 35, 52, 14, 56, 601, 777, 888, 999]) -> [11, 35, 601, 777, 999]
  */
 function getOdds(numbers) {
-  // Your code here
+  const Odds = [];
+  Odds.push(numbers.filter(num => num % 2 === 1));
+  return Odds;   
 }
+
+//console.log(getOdds([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
 
 /**
  * getEvens(numbers):
@@ -23,8 +28,12 @@ function getOdds(numbers) {
  * getEvens([11, 35, 52, 14, 56, 601, 777, 888, 999]) -> [52, 14, 56, 888]
  */
 function getEvens(numbers) {
-  // Your code here
+  const Evens = [];
+  Evens.push(numbers.filter(num => num % 2 === 0));
+  return Evens; 
 }
+//console.log(getEvens([11, 35, 52, 14, 56, 601, 777, 888, 999]));
+
 
 /**
  * countOccurences(x, numbers):
@@ -36,9 +45,12 @@ function getEvens(numbers) {
  * countOccurences(52, [11, 35, 52, 14, 56, 601, 52, 777, 888, 999, 52]) -> 3
  */
 function countOccurences(x, numbers) {
-  // Your code here
+  let occurence = [];
+  occurence.push(numbers.filter(cv => cv = x));
+  return occurence.length
 }
-
+//console.log(countOccurences(1, [1, 2, 3, 1, 4, 5, 6, 1, 7, 8, 9, 10, 11, 1, 12, 13]));
+//console.log(countOccurences(52, [11, 35, 52, 14, 56, 601, 52, 777, 888, 999, 52]));
 /**
  * makeThemDoctors(students):
  * - receives array `students`
@@ -49,7 +61,11 @@ function countOccurences(x, numbers) {
  */
 
 function makeThemDoctors(students) {
-  // Your code here
+  
+students.map(cv) {
+  cv = "Dr." + "" + students;
 }
+}
+console.log(makeThemDoctors(["Ali", "Aseel", "Richard"]));
 
 module.exports = { getOdds, getEvens, countOccurences, makeThemDoctors };
